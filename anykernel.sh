dca1093edd8a53a938fc71cc4a496abc58db8c06
LaKardo <kishenko2017@gmail.com>
2021-09-05 11:23:22 +0300

    Import files

---
+++ anykernel.sh
@@ -0,0 +1,34 @@
+# AnyKernel3 Ramdisk Mod Script
+# osm0sis @ xda-developers
+
+## AnyKernel setup
+# begin properties
+properties() { '
+do.devicecheck=1
+do.modules=0
+do.cleanup=1
+do.cleanuponabort=1
+device.name1=Redmi Note 8
+device.name2=Redmi Note 8T
+device.name3=ginkgo
+device.name3=willow
+supported.versions=11, 11.0
+supported.patchlevels=
+'; } # end properties
+
+# shell variables
+block=/dev/block/bootdevice/by-name/boot;
+is_slot_device=0;
+ramdisk_compression=auto;
+
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
+. tools/ak3-core.sh;
+
+## AnyKernel install
+dump_boot;
+
+# end ramdisk changes
+
+write_boot;
+## end install
