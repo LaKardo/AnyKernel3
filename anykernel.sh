08680c43cd8ac4ee0327d3eeaaddbc0715bb337b
LaKard0 <kishenko2017@gmail.com>
2022-10-22 10:32:09 +0300

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -6,20 +6,16 @@
 properties() { '
 do.devicecheck=1
 do.modules=0
-do.systemless=1
+do.systemless=0
 do.cleanup=1
 do.cleanuponabort=0
 device.name1=r8q
-device.name2=
-device.name3=
-device.name4=
-device.name5=
 supported.versions=
 supported.patchlevels=
 '; } # end properties
 
 # shell variables
-block=/dev/block/platform/soc/1d84000.ufshc/by-name/boot;
+block=/dev/block/bootdevice/by-name/boot;
 is_slot_device=0;
 ramdisk_compression=auto;
 no_block_display=1
